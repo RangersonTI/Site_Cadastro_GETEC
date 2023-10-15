6004579041d11b9189352bfa1f144a998b973c03
Rangerson Clemente <rangerson14@outlook.com>
2023-10-15 12:55:23 -0300

    update

--- regressive_Count_for_Getec.js
+++ regressive_Count_for_Getec.js
@@ -1,6 +1,6 @@
 // Contagem regressiva para o evento
 
-var countDownDate = new Date("October 15, 2023 10:26:00").getTime();  // Atualiza a contagem regressiva a cada 1 segundo 
+var countDownDate = new Date("October 15, 2023 15:00:00").getTime();  // Atualiza a contagem regressiva a cada 1 segundo 
 
 var x = setInterval(function() {                // Get todays date and time 
     var now = new Date().getTime();                      
@@ -14,12 +14,19 @@ var x = setInterval(function() {                // Get todays date and time
     
     // Apresenta o resultando do tempo restante na tela do usuario"
 
-    document.getElementById("regressive_Count").innerHTML = days + "dias " + hours + "horas " + minutes + "minutos " + seconds + "segundos ";
+    document.getElementById("dias").innerHTML = days;
+    document.getElementById("horas").innerHTML = hours;
+    document.getElementById("minutos").innerHTML = minutes;
+    document.getElementById("segundos").innerHTML = seconds;
  
     // Se o tempo restante acabar, sera escrito "TEMPO ESGOTAGO" 
     if (distance < 0) { 
-        clearInterval(x); 
-        document.getElementById("regressive_Count").innerHTML = "TEMPO ESGOTADO";
+        clearInterval(x);
+        document.getElementById("dias").innerHTML = 0;
+        document.getElementById("horas").innerHTML = 0;
+        document.getElementById("minutos").innerHTML = 0;
+        document.getElementById("segundos").innerHTML = 0;
+        //document.getElementById("regressive_Count").innerHTML = "TEMPO ESGOTADO";
         document.getElementById("button_local_cadastro").disabled = true;
     }
 }, 1000);

b62966512bc2bf288cb74199a02e65fa6a9385fd
Rangerson Clemente <rosa-clemente@hotmail.com>
2023-10-15 21:43:11 -0300

    Create configBD.php

---
+++ configBD.php
@@ -0,0 +1,17 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = 'wrede';
+    $dbName = 'PARTICIPANTES_GETEC';
+
+    $connection = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+    if($connection -> connect_errorno){
+        echo "Erro ao estabelecer conexão com banco de dados";
+    }
+    else{
+        echo "Conexão estabelecida com sucesso";
+    }
+
+?>
